a9780e3ad845d0c388ab07086d09c05dec7099ad
OLUWABORIOV <104468419+OLUWABORIOV@users.noreply.github.com>
2025-01-07 23:27:29 +0100

    commit

---
+++ src/Components/About.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+ function About() 
+{
+ return (
+    <>
+   <p>This is About page</p> 
+    </>
+ )
+}
+
+export default About;
---
+++ src/Components/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+ function Home() 
+{
+ return (
+    <>
+    <p>This is Home page</p>
+    </>
+ )
+}
+export default Home;
+
